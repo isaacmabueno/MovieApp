d3e8a7875b1de10018b690b95be5dd2a1b2eec58
isaacmabueno <isaacmabueno@gmail.com>
2019-04-14 22:10:28 -0500

    removing more code bloat and updating fetch from app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,69 +2,74 @@ import React, { Component } from "react";
 import Nav from './Nav';
 import Movie from "./Movie.js";
 
-const URL = "https://api.themoviedb.org/3/search/movie?api_key=";
 const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
-const language = "&language=en-US";
-const query = "&query=";
 
 class App extends Component {
     state = {
-      movies: [],
-      total_pages: null,
-      page_num: 1,
-      query: null
+        movies: [],
+        total_pages: null,
+        page_num: 1,
+        query: null
     };
 
   fetchMovies(search) {
-    fetch(URL + `${API_KEY}` + language + query + search + "&page=" + this.state.page_num)
-      .then(res => res.json())
-      .then(json => this.setState({ movies: json.results, total_pages: json.total_pages }));
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${search}&page=${this.state.page_num}` )
+          .then(res => res.json())
+          .then(json => this.setState({ movies: json.results, total_pages: json.total_pages }));
   }
 
   filterSearch = event => {
-    let term = event.target.value;
-    this.setState({
-      query: term
-    }, () => this.fetchMovies(this.state.query))
+      let term = event.target.value;
+      this.setState({
+        query: term
+      }, () => this.fetchMovies(this.state.query))
   };
 
   nextPage = () => {
-    if(this.state.movies && this.state.page_num < this.state.total_pages) {
-      this.setState({
-        page_num: this.state.page_num +=1
-      }, () => this.fetchMovies(this.state.query))
-    }
+      if(this.state.movies && this.state.page_num < this.state.total_pages) {
+          this.setState({
+              page_num: this.state.page_num +=1
+          }, () => this.fetchMovies(this.state.query))
+      }
   }
 
   previousPage = () => {
-    if(this.state.movies && this.state.page_num !== 1) {
-      this.setState({
-        page_num: this.state.page_num -=1
-      }, () => this.fetchMovies(this.state.query))
-    }
+      if(this.state.movies && this.state.page_num !== 1) {
+          this.setState({
+              page_num: this.state.page_num -=1
+          }, () => this.fetchMovies(this.state.query))
+      }
   }
 
   render() {
       return (
+        <div>
+          <Nav />
           <div>
-              <Nav />
-              <div>
-                  <div className="container">
-                      <div className="row">
-                          <section className="col s4">
-                              <input placeholder="Search Movie..." onChange={this.filterSearch}/>
-                          </section>
-                        </div>
+            <div className="container">
+               <div className="row">
+                    <section className="col s4">
+                        <input placeholder="Search Movie..." onChange={this.filterSearch}/>
+                    </section>
+                  </div>
             </div>
-            <button onClick={this.previousPage}style={{marginLeft: "28%"}}>Previous Page</button>
-            <button onClick={this.nextPage}style={{marginLeft: "0%"}}>Next Page >></button>
-        </div>
-        <div className="container">
-            <div className="row new">
-            { this.state.movies ? (this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)) : (null) }
+            <div className="container">
+                <div className="row">
+                    <section className="col s4">
+                        <ul className="pagination">
+                            <li className="waves-effect" onClick={this.previousPage}><i className="material-icons">chevron_left</i></li>
+                            <li className="waves-effect" onClick={this.nextPage}><i className="material-icons">chevron_right</i></li>
+                        </ul>
+                    </section>
+                </div>
             </div>
+          </div>
+          <div className="container">
+              <div className="row new">
+                { this.state.movies ? (this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)) : (null) }
+              </div>
+          </div>
         </div>
-      </div>
     );
   }
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,15 +9,8 @@ const Movie = (props) => {
             <div className="card-image waves-effect waves-light">
                 <img className="grid" alt={props.movie.poster_path} src={`${beginning}` + props.movie.poster_path}/>
             </div>
-            {/*<div className="card-content">
-                <span style={{ fontSize: "20px", textDecoration: "underline" }}><strong>{props.movie.title}</strong></span><br/>
-                <strong>Released: </strong> {props.movie.release_date} <br />
-                <strong>Popularity: </strong> {props.movie.popularity}
-                <p style={{ fontStyle: "italic" }}>{props.movie.overview}</p>
-    </div>*/}
         </div>
-    </div>
-                    
+    </div>             
     )
 };
 

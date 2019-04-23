6005f2c8c4a3b390df6723ecae4d35b212053561
isaacmabueno <isaacmabueno@gmail.com>
2019-04-22 20:34:54 -0500

    updating movie component removing grid from image and wrapping the whole div as well as updating the image size and removing unneccesary materialize classes to remove pointer

--- src/components/App.js
+++ src/components/App.js
@@ -11,24 +11,25 @@ class App extends Component {
         page_num: 1,
         query: null
     };
-
+// {/* FETCH REQUEST query gets passed in through here */}
   fetchMovies(search) {
       fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${search}&page=${this.state.page_num}`)
           .then(res => res.json())
           .then(json => this.setState({ movies: json.results, total_pages: json.total_pages }));
   }
-
+// {/* Filter function */}
   filterSearch = event => {
       let term = event.target.value;
       this.setState({
         query: term
+        // {/* Calls fetchMovies function with the query that's stored in the state, essentiall what you're typing in which is a live filter */}
       }, () => this.fetchMovies(this.state.query))
   };
 
   nextPage = () => {
       if(this.state.movies && this.state.page_num < this.state.total_pages) {
           this.setState({
-              page_num: this.state.page_num +=1
+              page_num: this.setState.page_num +=1
           }, () => this.fetchMovies(this.state.query))
       }
   }
@@ -36,7 +37,7 @@ class App extends Component {
   previousPage = () => {
       if(this.state.movies && this.state.page_num !== 1) {
           this.setState({
-              page_num: this.state.page_num -=1
+              page_num: this.setState.page_num -=1
           }, () => this.fetchMovies(this.state.query))
       }
   }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,14 +5,12 @@ const Movie = (props) => {
     return (        
     <div className="col s12 m6 l3">
         <div className="card">
-            <div className="card-image waves-effect waves-light">
-                <img className="grid" alt={props.movie.poster_path} src={`https://image.tmdb.org/t/p/original${props.movie.poster_path}`}/>
+            <div className="card-image">
+                <img alt={props.movie.poster_path} src={`https://image.tmdb.org/t/p/w342${props.movie.poster_path}`}/>
                 <span>{props.movie.title}</span>
             </div>
         </div>                  
     </div>  
-
-    
     )
 };
 

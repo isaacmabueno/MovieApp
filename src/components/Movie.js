d2c9b1a69e519ac131acefdab3e7a622708e1cd6
isaacmabueno <isaacmabueno@gmail.com>
2019-04-25 16:43:47 -0500

    updating the look at feel of the movie.js layout

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,7 +4,7 @@ import '../App.css'
 const Movie = (props) => {
     return (  
         <div className="row new">
-            <div className="col s12 m6 l4">
+            <div className="col s12 m6 l3">
                 <div className="card">
                     <div className="card-image">
                         <img alt={props.movie.poster_path} src={`https://image.tmdb.org/t/p/w342${props.movie.poster_path}`}/>
@@ -12,9 +12,10 @@ const Movie = (props) => {
                     </div>
                 </div>                  
             </div>
-            <div className="col s12 m6 l8">
-                <h1>{props.movie.title}</h1><br></br>
-                <span>{props.movie.overview}</span><br></br>
+            <div className="col s12 m6 l9 movie-info-area">
+                <h1>{props.movie.title}</h1>
+                <span>{props.movie.release_date}</span>
+                <p>{props.movie.overview}</p>
                 <p>Voter Average: {props.movie.vote_average}</p>
             </div>
         </div>
